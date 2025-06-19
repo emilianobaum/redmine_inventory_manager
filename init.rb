1cbf416209352866bff97b597721ee2803969739
Emiliano Baum <ebaum@conae.gov.ar>
2025-06-19 19:02:30 +0000

    Update init.rb para permitir que mas de un usuario pueda trabajar con el plugin.

--- init.rb
+++ init.rb
@@ -1,11 +1,16 @@
 Redmine::Plugin.register :redmine_inventory_manager do
   name 'Redmine Inventory Manager Plugin'
   author 'Daniel Anguita O.'
+  updated 'Emiliano Baum'
   description 'Take your warehouse or office inventory on the same platform of your projects'
   version '0.9'
   url 'https://github.com/danielanguita/Redmine-Inventory-Manager'
 
-  permission :inventory, {:inventory => [:index, :movements, :categories, :parts, :warehouses, :providors]}, :public => false
+  # permission :inventory, {:inventory => [:index, :movements, :categories, :parts, :warehouses, :providors]}, :public => false
+
+  permission :view_inventory, {:inventory => [:index, :movements]}
+  permission :manage_inventory_parts, {:inventory => [:parts, :categories, :warehouses]}
+  permission :manage_inventory_providors, {:inventory => [:providors]}
 
   menu :top_menu, :inventory, { :controller => 'inventory', :action => 'index' }, { :caption => 'Inventory', :before => 'admin'}
   	
